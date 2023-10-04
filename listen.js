b55edf117adf2a957debb1c41121f41377d5896f
James Yuill <jamesyuill@jamess-mbp.lan>
2023-10-04 10:22:33 +0100

    updates pinger function

--- listen.js
+++ listen.js
@@ -1,7 +1,11 @@
 const app = require('./app.js');
-
+const cronJob = require('./pinger/pinger.js');
 const port = process.env.PORT || 8080;
 
 app.listen(port, () => {
   console.log(`listening on port ${port}`);
 });
+
+if (process.env.NODE_ENV === 'production') {
+  cronJob().start();
+}
--- pinger/pinger.js
+++ pinger/pinger.js
@@ -1,5 +1,12 @@
 const cron = require('node-cron');
+const http = require('https');
 
-const cronJob = cron.schedule('0 */10 * * * *', function () {
-  console.log(`ping!`);
-});
+function cronJob() {
+  return cron.schedule('0 */10 * * * *', function () {
+    http.get('https://what-carb-be.onrender.com/', (res) => {
+      console.log('pinged!');
+    });
+  });
+}
+
+module.exports = cronJob;
